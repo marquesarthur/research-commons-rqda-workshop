da0c8ca887c45a0852417ccf7f7b2bf2c599eefb
Arthur Marques <marques.s.arthur@gmail.com>
2020-03-02 22:14:59 -0800

    new install script

---
+++ install.R
@@ -0,0 +1,12 @@
+if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("tm")) install.packages("tm")
+if (!require("wordcloud")) install.packages("wordcloud")
+if (!require("topicmodels")) install.packages("topicmodels")
+if (!require("ggplot2")) install.packages("ggplot2")
+if (!require("tidytext")) install.packages("tidytext")
+if (!require("dplyr")) install.packages("dplyr")
+if (!require("tidyr")) install.packages("tidyr")
+if (!require("sentimentr")) install.packages("sentimentr")
+
+
+if (!require("RQDA")) install.packages("RQDA")
